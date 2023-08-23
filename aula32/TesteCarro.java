7e93f366d3bb2987373e8a734ddf5d81f11b4301
IgorTecnologic <igorgcf@outlook.com.br>
2023-08-23 16:37:37 -0300

    feat: Orientação a objetos; Encapsulamento: métodos getters e setters

---
+++ aula32/TesteCarro.java
@@ -0,0 +1,16 @@
+package com.igor.cursojava.aula32;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro van = new Carro();
+		van.setMarca("Fiat");
+		van.setModelo("Ducato");
+		System.out.println(van.getMarca());
+		System.out.println(van.getModelo());
+		
+	
+	}
+
+}
