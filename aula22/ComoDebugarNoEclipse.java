3542f25b1ee16482e14314760910be2ac4c6237a
IgorTecnologic <igorgcf@outlook.com.br>
2023-08-23 16:13:35 -0300

    feat: Como debugar no eclipse

---
+++ aula22/ComoDebugarNoEclipse.java
@@ -0,0 +1,14 @@
+package com.igor.cursojava.aula22;
+
+public class ComoDebugarNoEclipse {
+
+	public static void main(String[] args) {
+		
+		int [] notas = new int [3];
+
+		for (int i =0; i<4; i++) {
+			notas[i] =i;
+		}
+	}
+
+}
