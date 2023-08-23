3ea3d4684e6c91ec446ff642df6f5ce748572858
IgorTecnologic <igorgcf@outlook.com.br>
2023-08-23 16:27:53 -0300

    feat: Orientação a objetos; Pacotes e import

---
+++ aula28/Pacote.java
@@ -0,0 +1,21 @@
+package com.igor.cursojava.aula28;
+
+
+
+import com.igor.cursojava.aula20Licao.*; // com '*' tenho acesso a todas as classes desse pacote.
+import com.igor.cursojava.aula20Licao.Exer06JogoDaVelha;
+import com.igor.cursojava.aula27.Carro;
+
+public class Pacote {
+
+	public static void main(String[] args) {
+		
+		Carro carro;
+		com.igor.cursojava.aula26.Carro  carro2;
+
+		Exer06JogoDaVelha exer06JogoDaVelha ;
+		Exer05 exer05;
+		Exer04 exer04;
+	}
+
+}
