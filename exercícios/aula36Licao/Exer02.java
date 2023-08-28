e6fca34a2dd7d424355d9a824bdbc73f3e8e14a5
IgorTecnologic <igorgcf@outlook.com.br>
2023-08-27 22:41:12 -0300

    feat: Exercícios aula 36

---
+++ exercícios/aula36Licao/Exer02.java
@@ -0,0 +1,74 @@
+package com.igor.cursojava.aula36Licao;
+
+import java.util.Scanner;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Entre com o nome do curso: ");
+	String nome = scan.nextLine();
+	
+	System.out.println("Entre com o horário do curso:");
+	String horario = scan.nextLine();
+
+	System.out.println("Entre com o nome do professor:");
+	String nomeProf = scan.nextLine();
+	
+	System.out.println("Entre com o departamento do pprofessor:");
+	String dep = scan.nextLine();
+	System.out.println("Entre com o email do professor: ");
+	String emailProf = scan.nextLine();
+	
+	Curso curso = new Curso();
+	curso.setNome(nome);
+	curso.setHorario(horario);
+	
+	Professor professor = new Professor();
+	
+	professor.setNome(nomeProf);
+	professor.setDepartamento(dep);
+	professor.setEmail(emailProf);
+	
+	curso.setProfessor(professor);
+	
+	System.out.println("-------- Alunos-------");
+	
+	Aluno [] alunos = new Aluno [5];
+	
+	
+	for (int i=0; i<5; i++){
+		
+		scan.nextLine();
+		
+		System.out.println("Entre com o nome do aluno " + (i+1));
+		String nomeAluno = scan.nextLine();
+		
+		System.out.println("Entre com a matricula do aluno: ");
+		String matAluno = scan.nextLine();
+		
+		double [] notas = new double [4];
+		
+		for ( int j =0; j <4; j++) {
+			System.out.println("Entre com a nota: " + (j+1));
+			notas[j] = scan.nextDouble();
+		}
+		
+		Aluno aluno = new Aluno();
+		aluno.setNome(nomeAluno);
+		aluno.setMatricula(matAluno);
+		aluno.setNotas(notas);
+		
+		alunos[i] = aluno;
+	}
+		
+		
+		curso.setAlunos(alunos);
+		
+		System.out.println(curso.obterInfo());
+		
+	}
+	}
+	
