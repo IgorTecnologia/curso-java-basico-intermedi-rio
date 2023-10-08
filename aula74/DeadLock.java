e38edf6ab54198eab9292e1fb5068fd50e4a2dfa
IgorTecnologic <igorgcf@outlook.com.br>
2023-10-08 17:48:13 -0300

    Threads: deadlock

---
+++ aula74/DeadLock.java
@@ -0,0 +1,59 @@
+package com.igor.cursojava.aula74;
+
+public class DeadLock {
+
+	public static void main(String[] args) {
+
+		final String RECURSO1 = "Recurso #1";
+		final String RECURSO2 = "Recurso #2";
+
+		Thread t1 = new Thread() {
+			public void run() {
+				synchronized(RECURSO1) {
+					System.out.println("Thread #1: bloqueou recurso 1");
+
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println("Thread #1: tentando o acesso ao recurso 2");
+
+					synchronized (RECURSO2) {
+						System.out.println("Thread #1: bloqueou recurso 2");
+					}
+					
+					}
+				}
+			};
+
+			Thread t2 = new Thread() {
+				public void run() {
+					synchronized(RECURSO2) {
+						System.out.println("Thread #2: bloqueou recurso 2");
+
+						try {
+							Thread.sleep(100);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						System.out.println("Thread #2: tentando o acesso ao recurso 1");
+
+						synchronized (RECURSO1) {
+							System.out.println("Thread #2: bloqueou recurso 1");
+
+						
+					}
+				}
+			}
+		};		
+		
+		t1.start();
+		t2.start();
+	}
+}
+		
+	
+			
+		
+		
